98738ec4e21be167edfc81a5bdbecfcc6353db0e
Huynhlehoan <22682941.hoan@student.iuh.edu.vn>
2025-11-26 10:39:57 +0700

    chore: update account & employee controller/service for FE usage

--- src/main/java/iuh/fit/se/cosmeticsecommercebackend/repository/AccountRepository.java
+++ src/main/java/iuh/fit/se/cosmeticsecommercebackend/repository/AccountRepository.java
@@ -2,9 +2,10 @@ package iuh.fit.se.cosmeticsecommercebackend.repository;
 
 import iuh.fit.se.cosmeticsecommercebackend.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, Long>, JpaSpecificationExecutor<Account> {
 
 }
