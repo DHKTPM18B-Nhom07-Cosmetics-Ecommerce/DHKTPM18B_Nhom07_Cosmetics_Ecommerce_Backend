4cdd44537361a40597a3fb1ced0149d8fac47f6d
Hong Minh <nguyengochongminh@gmail.com>
2025-12-06 22:10:28 +0700

    them pt countOrdersByStatusAndDate cho repository cho Hoan

--- src/main/java/iuh/fit/se/cosmeticsecommercebackend/repository/OrderRepository.java
+++ src/main/java/iuh/fit/se/cosmeticsecommercebackend/repository/OrderRepository.java
@@ -7,6 +7,7 @@ import iuh.fit.se.cosmeticsecommercebackend.model.enums.OrderStatus;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -43,4 +44,10 @@ public interface OrderRepository extends JpaRepository<Order, String> {
      */
     @Query(value = "SELECT o.order_id FROM orders o WHERE o.order_id LIKE CONCAT(?1, '%') ORDER BY o.order_id DESC LIMIT 1", nativeQuery = true)
     Optional<String> findLastOrderIdByDatePrefix(String prefix);
+    @Query("SELECT COUNT(o) FROM Order o WHERE o.customer.account.id = :accountId AND o.status = :status AND o.orderDate >= :startTime")
+    long countOrdersByStatusAndDate(
+            @Param("accountId") Long accountId,
+            @Param("status") OrderStatus status,
+            @Param("startTime") LocalDateTime startTime
+    );
 }
