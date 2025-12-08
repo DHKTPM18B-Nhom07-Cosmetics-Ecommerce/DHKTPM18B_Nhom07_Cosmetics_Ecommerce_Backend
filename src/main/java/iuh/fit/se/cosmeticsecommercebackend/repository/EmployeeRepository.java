f643b8e042f803262886a91ed00742fbe1544be6
NguyenHoa22643291 <22643291.hoa@student.iuh.edu.vn>
2025-12-08 12:15:52 +0700

    feat: add employee retrieval by account username and enhance order processing with guest phone support

--- src/main/java/iuh/fit/se/cosmeticsecommercebackend/repository/EmployeeRepository.java
+++ src/main/java/iuh/fit/se/cosmeticsecommercebackend/repository/EmployeeRepository.java
@@ -2,8 +2,15 @@ package iuh.fit.se.cosmeticsecommercebackend.repository;
 
 import iuh.fit.se.cosmeticsecommercebackend.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    // Tìm Employee bằng Username của Account liên kết
+    @Query("SELECT e FROM Employee e JOIN e.account a WHERE a.username = :username")
+    Optional<Employee> findByAccountUsername(@Param("username") String username);
 }
